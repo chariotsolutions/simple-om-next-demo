4301bb3392b55bcd5d961d981c615dc4778decfe
Ron Lusk <rlusk@chariotsolutions.com>
2016-10-06 12:35:09 -0400

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject krimple-react "0.1.1-SNAPSHOT"
+(defproject krimple-react "0.1.1"
   :description "Minimal Om.Next/React demo program"
   :url "https://github.com/chariotsolutions/simple-om-next-demo"
   :license {:name "Eclipse Public License"
